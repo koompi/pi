b17fbff3476707c6f1d5479fa1d74b994d648a34
mrrbrilliant <mrrbrilliantwork@gmail.com>
2021-03-30 11:08:20 +0700

    fixed extraction error dued to missing output path

--- src/utils/decompress.rs
+++ src/utils/decompress.rs
@@ -1,16 +1,17 @@
 use crate::statics::*;
 use crate::utils::{extract_archive, extract_zip};
 use bzip2::read::BzDecoder;
+use colored::Colorize;
 use flate2::read::GzDecoder;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::{
-    fs::{create_dir_all, File},
+    fs::{create_dir_all, File, OpenOptions},
     io::*,
     path::PathBuf,
     process::{Command, Stdio},
     str,
 };
-use tar::Archive;
+use tar::{Archive, Unpacked};
 use xz2::read::XzDecoder;
 use zstd::Decoder;
 
@@ -28,36 +29,92 @@ pub fn decompress_all(source: &str, dest: &str) -> Result<()> {
             let tar = BzDecoder::new(file);
             let mut archive = Archive::new(tar);
 
+            let pb = ProgressBar::new_spinner();
+            pb.enable_steady_tick(100);
+            pb.set_style(
+                ProgressStyle::default_spinner()
+                    .tick_chars("/|\\- ")
+                    .template("{spinner:.green.bold} Extracting: {wide_msg}"),
+            );
+
             for file in archive.entries().unwrap() {
                 let mut file = file.unwrap();
 
-                file.unpack(format!("{}/{}", dest, file.path().unwrap().display()))
-                    .unwrap();
+                let new_file_path = dest_path.join(&file.path().unwrap());
+                let new_dir_path = new_file_path.clone();
+
+                if !new_dir_path.parent().unwrap().exists() {
+                    create_dir_all(new_dir_path.parent().unwrap());
+                }
+                file.set_unpack_xattrs(true);
+                match file.unpack(new_file_path) {
+                    Ok(d) => pb.set_message(&format!("{:?}", &file.path().unwrap())),
+                    Err(e) => println!("{}", &e.to_string().red()),
+                }
             }
+            pb.finish();
         }
         "gz" => {
-            let file = File::open(source)?;
+            // let file = File::open(source)?;
+            let mut option = OpenOptions::new();
+            let file = option.read(true).write(false).open(source)?;
             let tar = GzDecoder::new(file);
             let mut archive = Archive::new(tar);
 
+            let pb = ProgressBar::new_spinner();
+            pb.enable_steady_tick(100);
+            pb.set_style(
+                ProgressStyle::default_spinner()
+                    .tick_chars("/|\\- ")
+                    .template("{spinner:.green.bold} Extracting: {wide_msg}"),
+            );
+
             for file in archive.entries().unwrap() {
                 let mut file = file.unwrap();
 
-                file.unpack(format!("{}/{}", dest, file.path().unwrap().display()))
-                    .unwrap();
+                let new_file_path = dest_path.join(&file.path().unwrap());
+                let new_dir_path = new_file_path.clone();
+
+                if !new_dir_path.parent().unwrap().exists() {
+                    create_dir_all(new_dir_path.parent().unwrap());
+                }
+                file.set_unpack_xattrs(true);
+                match file.unpack(new_file_path) {
+                    Ok(d) => pb.set_message(&format!("{:?}", &file.path().unwrap())),
+                    Err(e) => println!("{}", &e.to_string().red()),
+                }
             }
+            pb.finish();
         }
         "xz" => {
             let file = File::open(source)?;
             let tar = XzDecoder::new(file);
             let mut archive = Archive::new(tar);
 
+            let pb = ProgressBar::new_spinner();
+            pb.enable_steady_tick(100);
+            pb.set_style(
+                ProgressStyle::default_spinner()
+                    .tick_chars("/|\\- ")
+                    .template("{spinner:.green.bold} Extracting: {wide_msg}"),
+            );
+
             for file in archive.entries().unwrap() {
                 let mut file = file.unwrap();
 
-                file.unpack(format!("{}/{}", dest, file.path().unwrap().display()))
-                    .unwrap();
+                let new_file_path = dest_path.join(&file.path().unwrap());
+                let new_dir_path = new_file_path.clone();
+
+                if !new_dir_path.parent().unwrap().exists() {
+                    create_dir_all(new_dir_path.parent().unwrap());
+                }
+                file.set_unpack_xattrs(true);
+                match file.unpack(new_file_path) {
+                    Ok(d) => pb.set_message(&format!("{:?}", &file.path().unwrap())),
+                    Err(e) => println!("{}", &e.to_string().red()),
+                }
             }
+            pb.finish();
         }
         "zip" => extract_zip(file_path.to_str().unwrap(), dest).unwrap(),
         "zst" => {
