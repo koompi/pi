3152b34b1faf7a837ef339bb18b4580ea8ce259d
mrrbrilliant <mrrbrilliantwork@gmail.com>
2021-09-20 12:27:38 +0700

    skip solvent error

--- src/bin_database.rs
+++ src/bin_database.rs
@@ -98,7 +98,10 @@ impl BinDatabase {
             for package in packages.iter() {
                 if let Ok(nodes) = rd.dependencies_of(package) {
                     for node in nodes {
-                        to_install_name.push(node.unwrap().to_string())
+                        // to_install_name.push(node.unwrap().to_string())
+                        if let Ok(node) = node {
+                            to_install_name.push(node.to_string())
+                        }
                     }
                 }
             }
