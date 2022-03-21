acedc1363ef06c38f70743d9e23748d9deb1d519
mrrbrilliant <mrrbrilliantwork@gmail.com>
2022-03-21 10:24:21 +0700

    update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin pi
-env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin bin-repo
-env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin server
-env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin source-repo
+if [[ $1 == "musl" ]]; then
+    env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin pi
+    env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin bin-repo
+    env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin server
+    env RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --bin source-repo
+else
+    cargo build --release --bin pi
+    cargo build --release --bin bin-repo
+    cargo build --release --bin server
+    cargo build --release --bin source-repo
+fi
 
 sudo install -Dm755 target/release/pi /usr/bin/pi
 sudo install -Dm755 target/release/bin-repo /usr/bin/bin-repo
